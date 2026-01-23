15569c86a5757bdb440008387378c84f796f9265
giovannimazzi <gioelor@libero.it>
2026-01-23 02:47:06 +0100

    inserita gestione pulsante

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 // parametro esercizio
 const cardinality = 10;
 
+// pulsante per rigenerare la lista
+const refreshListButton = document.getElementById("ref-list-btn");
+
 // generazione lista mail al caricamento della pagina
-requestNEmailAddresses(cardinality);
+requestNEmailAddresses(cardinality, refreshListButton);
+
+// click sul pulsante per rigenerare la lista
+refreshListButton.addEventListener("click", () => {
+  requestNEmailAddresses(cardinality, refreshListButton);
+});
 
 // funzione che gestisce le richieste API
-function requestNEmailAddresses(n) {
+function requestNEmailAddresses(n, refButtonElement) {
   // elementi html coinvolti
   const parent = document.getElementById("list");
   if (!parent) return;
   const loadingBar = document.getElementById("loading-bar");
   if (!loadingBar) return;
+
+  if (refButtonElement) refreshListButton.disabled = true;
 
   // API Boolean mail
   const apiUrl = `https://flynn.boolean.careers/exercises/api/random/mail`;
@@ -36,7 +46,7 @@ function requestNEmailAddresses(n) {
       .get(apiUrl)
       .then((result) => {
         li.classList.add("h4");
-        li.innerHTML = `<a class="text-decoration-none" href="mailto:${result.data.response}">📨  ${result.data.response}</a>`;
+        li.innerHTML = `<a class="text-decoration-none text-nowrap" href="mailto:${result.data.response}">📨  ${result.data.response}</a>`;
       })
       .catch((error) => {
         console.log(`%cERROR:%c ${error}`, "color: orangered", "");
@@ -50,6 +60,7 @@ function requestNEmailAddresses(n) {
           setTimeout(() => {
             clearInterval(loadID);
             loadingBar.classList.add("d-none");
+            if (refButtonElement) refButtonElement.disabled = false;
           }, 1500);
         }
       });
